8afd58be46bf80791b96979a6f4e92aae5c84241
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-15 13:05:02 +0800

    update: layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,14 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main className="w-[100dvw] h-[100dvh] grid items-center justify-center">
-      <div className="flex gap-16">
+      <div className="flex gap-16 w-full">
         <Image src="https://avatars.githubusercontent.com/u/98001197?v=4" alt="avatar" width="256" height="256" className="rounded-full" />
-        <div className="grid gap-4 items-center w-80 my-8">
+        <div className="gap-4 items-center my-8 hidden lg:grid">
           <h1>Loki</h1>
           <h6>詹詠翔</h6>
-          <p>大三的小廢物, 電子系, 在這分享一些自身所學與經驗.</p>
+          <p>大三的小廢物, 電子系, 在這分享一些自身所學與經驗</p>
         </div>
       </div>
-      {/* <Separator orientation="vertical" className="m-auto" /> */}
     </main>
   )
 }
