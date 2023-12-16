3e33ad8cc5a6e27c570db771eaeab3b22adde434
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-17 01:56:03 +0800

    update: prisma and oauth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import prisma from "@/lib/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
