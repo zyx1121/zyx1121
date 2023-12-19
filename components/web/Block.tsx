24e7ddb12561f41ad4cda290722c9b636c843ce6
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-20 00:36:28 +0800

    update: style

--- components/web/Block.tsx
+++ components/web/Block.tsx
@@ -1,14 +1,44 @@
+"use client"
+
+import { animated, useSpring } from "@react-spring/web"
 import { format, parseISO } from "date-fns"
 
 export function Block({ title, date }: { title: string, date: string }) {
+  const [springs, api] = useSpring(() => ({
+    from: { paddingLeft: "1rem" },
+  }))
+
+  const mouseEnter = () => {
+    api.start({
+      from: { paddingLeft: springs.paddingLeft },
+      to: { paddingLeft: "1.5rem" },
+    })
+  }
+
+  const mouseLeave = () => {
+    api.start({
+      from: { paddingLeft: springs.paddingLeft },
+      to: { paddingLeft: "1rem" },
+    })
+  }
+
   return (
-    <div className="border p-4 rounded-lg hover:border-border border-transparent cursor-pointer">
-      <h3>
-        {title}
-      </h3>
-      <time className="block text-muted-foreground text-sm" dateTime={date}>
-        {format(parseISO(date), 'yyyy年 L月 d日')}
-      </time>
+    <div className="p-4 cursor-pointer">
+      <div className="w-80 h-60 border border-border rounded-lg">
+
+      </div>
+      <animated.div
+        style={{ padding: "1rem", ...springs }}
+        onMouseEnter={() => mouseEnter()}
+        onMouseLeave={() => mouseLeave()}
+      >
+        <h3>
+          {title}
+        </h3>
+        <time className="block text-muted-foreground text-sm" dateTime={date}>
+          {format(parseISO(date), 'yyyy年 L月 d日')}
+        </time>
+      </animated.div>
     </div>
   )
 }
