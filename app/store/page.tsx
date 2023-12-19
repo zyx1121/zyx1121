29947b39524392d595c85c52249404f7974af74b
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-20 00:37:44 +0800

    update: coming soon

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -1,5 +1,7 @@
 export default function Store() {
   return (
-    <></>
+    <main className="w-full h-[100dvh] grid items-center justify-center text-muted-foreground">
+      敬啟期待
+    </main>
   )
 }
