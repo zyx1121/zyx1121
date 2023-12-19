fd9c214d68be24d2a2e597cf82a48f383f6171a7
zyx1121 <yongxiang.zhan@outlook.com>
2023-12-20 00:37:15 +0800

    update: content

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <Image src="https://avatars.githubusercontent.com/u/98001197?v=4" alt="avatar" width="256" height="256" className="rounded-full" />
         <div className="gap-4 items-center my-8 hidden lg:grid">
           <h1>Loki</h1>
-          <h6>詹詠翔</h6>
-          <p>大三的小廢物, 電子系, 在這分享一些自身所學與經驗</p>
+          <p>詹詠翔</p>
+          <p>彰化人, 台科大電子系三年級, 在這分享一些有的沒的</p>
         </div>
       </div>
     </main>
