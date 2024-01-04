27460a67dbc950c28d87c93965f5359233b651de
zyx1121 <yongxiang.zhan@outlook.com>
2024-01-04 15:31:39 +0800

    fix: adapter 無法使用 暫時刪除

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,6 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 const handler = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
