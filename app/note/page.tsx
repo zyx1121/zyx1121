d27440df3d8f876c74366aa4c05cd055ab2ceabd
zyx1121 <yongxiang.zhan@outlook.com>
2024-01-10 13:59:44 +0800

    style: 改動卡片之間的 padding

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -8,8 +8,8 @@ export default async function Note() {
   let allBlogs = getBlogPosts()
 
   return (
-    <main className="w-[100dvw] h-[100dvh] grid items-center justify-center">
-      <section>
+    <main className="w-full h-auto grid items-center justify-center">
+      <section className="mt-20">
         {allBlogs
           .sort((a, b) => {
             if (new Date(a.metadata.date) > new Date(b.metadata.date))
@@ -21,12 +21,12 @@ export default async function Note() {
             <Link key={post.slug} href={`/note/${post.slug}`}>
               {/* <Block title={post.metadata.title} date={post.metadata.date} /> */}
               <CardContainer className="inter-var">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-2xl p-6 border  ">
+                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-2xl p-6 border m-6">
                   <CardItem translateZ="100" className="w-full mb-4">
                     <Image
                       src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                      height="1000"
-                      width="1000"
+                      height="500"
+                      width="500"
                       className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                       alt="thumbnail"
                     />
